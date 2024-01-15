b0cc7f015ee65dd801d9d3c9a515f0b955427df5
Victor <unicaxverdad@gmail.com>
2024-01-15 08:55:13 +0300

    coins, ui

--- Assets/Code/UI/Views/HUDView.cs
+++ Assets/Code/UI/Views/HUDView.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using TMPro;
 using UniRx;
 using UnityEngine;
 
@@ -6,21 +6,29 @@ namespace UI.Views
 {
     public class HUDView : MonoBehaviour
     {
-        private List<GameObject> _spawnedLives = new();
+        [SerializeField] private TextMeshProUGUI _lives;
+        [SerializeField] private TextMeshProUGUI _coins;
 
         public struct Ctx
         {
             public ReactiveProperty<int> lives;
+            public ReactiveProperty<int> coins;
         }
 
         public void SetContext(Ctx ctx)
         {
             ctx.lives.Subscribe(OnLivesChanges).AddTo(this);
+            ctx.coins.Subscribe(OnLivesChanges).AddTo(this);
         }
-
 
         private void OnLivesChanges(int newValue)
         {
+            _lives.text = newValue.ToString();
+        }
+
+        private void OnCoinsChanges(int newValue)
+        {
+            _coins.text = newValue.ToString();
         }
     }
 }
