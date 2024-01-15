ad7e4c6bab06808268bbc4ff4cff16b820f2a8c9
Victor <unicaxverdad@gmail.com>
2024-01-15 05:17:46 +0300

    + new coins

---
+++ Assets/Code/Items/Coin.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Items
+{
+    public class Coin : Item
+    {
+        [Space, SerializeField] private float _rotationSpeed = 50f;
+
+        private void Update()
+        {
+            transform.Rotate(Vector3.up * _rotationSpeed * Time.deltaTime, Space.World);
+        }
+    }
+}
