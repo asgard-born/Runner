491ded35f7b30f98b7c5888da5ce66bb6e4fb477
Victor <unicaxverdad@gmail.com>
2024-01-16 04:22:05 +0300

    soundplayer & music

---
+++ Assets/Code/Configs/SoundConfigs.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Configs
+{
+    /// <summary>
+    /// Отведенная область для хранения звуков и музыки. Список звуков, которые будут
+    /// добавляться, можно держать в словаре по отдельным ключам enum и запускать по этим же ключам
+    /// </summary>
+    [CreateAssetMenu(menuName = "Configs/Audio Configs", fileName = "Audio_Configs")]
+    public class AudioConfigs : ScriptableObject
+    {
+        [SerializeField] private List<AudioClip> _musicList;
+        
+        public List<AudioClip> musicList => _musicList;
+    }
+}
---
+++ Assets/Code/SoundPlayerPm.cs
@@ -0,0 +1,44 @@
+﻿using Configs;
+using Framework;
+using Framework.Reactive;
+using UniRx;
+using UnityEngine;
+
+public class SoundPlayerPm : BaseDisposable
+{
+    private readonly Ctx _ctx;
+
+    public struct Ctx
+    {
+        public ReactiveTrigger onGameRun;
+        public AudioConfigs configs;
+        public AudioSource audioSource;
+    }
+
+    public SoundPlayerPm(Ctx ctx)
+    {
+        _ctx = ctx;
+
+        AddUnsafe(_ctx.onGameRun.Subscribe(StartPlayingMusicProcess));
+    }
+
+    private void StartPlayingMusicProcess()
+    {
+        AddUnsafe(Observable.EveryUpdate().Subscribe(PlayingMusicProcess));
+    }
+
+    private void PlayingMusicProcess(long _)
+    {
+        if (!_ctx.audioSource.isPlaying)
+        {
+            PlayRandomSound();
+        }
+    }
+
+    private void PlayRandomSound()
+    {
+        var clips = _ctx.configs.musicList;
+        var randomIndex = Random.Range(0, clips.Count);
+        _ctx.audioSource.PlayOneShot(clips[randomIndex]);
+    }
+}
